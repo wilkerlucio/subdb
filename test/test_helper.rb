9f498a677bfc8b9d5943d7e132dff092abbe18e7
Wilker Lucio <wilkerlucio@gmail.com>
2011-05-06 13:49:53 -0300

    generate test coverage on run tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,6 +19,9 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+require 'simplecov'
+SimpleCov.start
+
 require 'test/unit'
 require 'mocha'
 require 'subdb'
